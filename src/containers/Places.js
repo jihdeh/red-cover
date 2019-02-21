8491a4f0e36b39ab8d104b51214b55154237a906
jihdeh <jide.b.tade@gmail.com>
2019-02-21 14:47:54 +0100

    use class to define active state on selected

--- src/containers/Places.js
+++ src/containers/Places.js
@@ -8,9 +8,9 @@ import { faEllipsisH } from "@fortawesome/free-solid-svg-icons";
 const Places = () => {
   const context = useContext(Context);
   const {
-      updateState,
-      state: { selectedPlaces }
-    } = context;
+    updateState,
+    state: { selectedPlaces }
+  } = context;
 
   function handleChange({ target }) {
     const placeName = target.name;
@@ -25,13 +25,17 @@ const Places = () => {
   }
 
   function renderPlaces(place) {
+    const hasPlaceSelectedClass = selectedPlaces.has(place.imageName)
+      ? "place--container-active"
+      : "place--container";
+
     return (
       <div
         key={place.imageName}
         className=" col-xs-6 col-sm-4 col-md-5ths col-lg-5ths"
       >
-        <div className="d-flex flex-column place--container">
-          <div>
+        <div className={`d-flex flex-column ${hasPlaceSelectedClass}`}>
+          <div className="place--action-detail">
             <div className="place--checkbox">
               <Checkbox
                 name={place.imageName}
@@ -47,14 +51,14 @@ const Places = () => {
           <div>
             <button
               type="button"
-              className="btn btn-lg position-absolute place--view__button"
+              className="btn btn-lg position-absolute place--view__button place--action-detail"
             >
               View
             </button>
           </div>
           <img
             alt={place.imageName}
-            className="img-fluid rounded place--img w-100"
+            className="img-fluid rounded w-100 place--img"
             src={place.thumbnail}
           />
           <div className="place--detail">
